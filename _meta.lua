38a1279e0e060ae9d5c15d3086083cb97fcbf15f
rsrax <rs.wolviepool1499@gmail.com>
2025-11-16 18:18:44 +0000

    first commit

---
+++ _meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "opdscovers",
+    fullname = _("OPDS with Covers"),
+    description = _([[OPDS catalog browser with book cover display support. Browse and download books from online catalogs with visual cover previews.]]),
+}
