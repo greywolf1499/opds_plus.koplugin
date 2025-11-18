262c10817e4472fd399e19cd3303514fd8724b97
rsrax <rs.wolviepool1499@gmail.com>
2025-11-18 16:00:17 +0000

    fix: Update version to use dynamic versioning from the version module

--- _meta.lua
+++ _meta.lua
@@ -1,7 +1,8 @@
 local _ = require("gettext")
+local V = require("version")
 return {
     name = "opdsplus",
     fullname = _("OPDS Plus"),
-    version = "1.1.0",
+    version = V.VERSION,
     description = _([[OPDS catalog browser with book cover display support. Browse and download books from online catalogs with visual cover previews in list or grid view.]]),
 }
